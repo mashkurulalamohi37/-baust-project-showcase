00747f30413101c678667a0a1b702f845e934c52
mashkurulalamohi37 <68511087+mashkurulalamohi37@users.noreply.github.com>
2026-02-21 12:59:53 +0000

    Deploying to gh-pages from @ mashkurulalamohi37/-baust-project-showcase@ff46879d091b1f0e12ce8cae4a833bf8efbd559c 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "723006144" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3759552007" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
